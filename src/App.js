ebcda5dfb9e887e9b75d3bf7dfe84f6fce51925a
liyaSileshi <liyasileshi@gmail.com>
2020-04-23 23:43:00 -0700

    persisting timers

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import reducers from './reducers'
 import {update} from './actions'
 import NewTimer from './components/new-timer'
 import ListTimer from './components/list-timers'
+import {loadState, saveState} from './utils'
+import throttle from 'lodash/throttle'
 
-const store = createStore(reducers,
+const persistedState = loadState()
+
+const store = createStore(reducers, persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
+//saves state from local storage to redux store
+//throttle makes the subscribe run every 1000ms instead of 50ms
+store.subscribe(throttle(() => {
+  saveState(store.getState())
+}, 1000));
 
 let lastUpdateTime = Date.now()
 setInterval (() => {
